2ad4bdc7982401def3179966fa580f6cc2b7b1e8
KarolinaBrzezinska <91696944+KarolinaBrzezinska@users.noreply.github.com>
2022-04-10 14:45:28 +0200

    update 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const $startButton = document.querySelector('button[data-start');
+const $stopButton = document.querySelector('button[data-stop');
+const $body = document.querySelector('body');
+let timerID = null;
 
+$startButton.disabled = false;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const settingRandomBackgroundColor = (targetName) => {
+    targetName.style.backgroundColor = getRandomHexColor();
+}
+
+
+$startButton.addEventListener("click", () => {
+    $startButton.disabled = true;
+    timerID = setInterval(() => settingRandomBackgroundColor($body), 1000);
+})
+$stopButton.addEventListener("click", () => {
+    clearInterval(timerID);
+})
